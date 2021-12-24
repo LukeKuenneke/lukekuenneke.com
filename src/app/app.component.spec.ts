c96ddf8a515132ae060336359fffccff5dca885f
Luke Kuenneke <Luke.Kuenneke@gmail.com>
2021-12-24 02:56:48 -0500

    Remove unused test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('lukekuenneke.com');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('lukekuenneke.com app is running!');
-  });
 });
