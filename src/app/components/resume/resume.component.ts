bdf9226db09724655e405672e4d208ccd8524cad
Luke Kuenneke <luke.kuenneke@gmail.com>
2021-12-30 07:41:37 +0000

    Update Resume Loading.

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
@@ -6,13 +6,13 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
-export class ResumeComponent implements OnInit {
+export class ResumeComponent implements AfterViewInit {
   // GDrive File Link: https://docs.google.com/uc?id=1UEY-sB2py8bNRuhru1bc5eV7IUXAwAB8
   public RESUME_URL = 'https://drive.google.com/file/d/1UEY-sB2py8bNRuhru1bc5eV7IUXAwAB8/view?usp=sharing';
 
   constructor(@Inject(DOCUMENT) private document: Document) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.document.location.href = this.RESUME_URL;
   }
 }
