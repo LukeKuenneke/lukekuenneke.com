6cfa0a153b59751cf3ab28d4635470bb4389a13d
Luke Kuenneke <Luke.Kuenneke@gmail.com>
2024-09-28 00:25:58 -0400

    Update dependencies and make carousel smoother.

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import * as testimonialFile from '../../../assets/data/testimonials.json';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
 import * as _ from "lodash";
+
+import * as testimonialFile from '../../../assets/data/testimonials.json';
+
 
 @Component({
   selector: 'app-testimonials',
   templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.scss']
+  styleUrls: ['./testimonials.component.scss'],
+  providers: [NgbCarouselConfig]
 })
+
 export class TestimonialsComponent implements OnInit {
   testimonialData: any = testimonialFile;
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.animation = false;
+  }
 
   ngOnInit(): void {
     this.testimonialData = _.shuffle(this.testimonialData.testimonials);
