49492af94e676874a24b0931df168916f80f16f6
Luke Kuenneke <Luke.Kuenneke@gmail.com>
2024-10-15 02:19:26 -0400

    Simplify Resume Page

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,18 +1,10 @@
-import { AfterViewInit, Component, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
-export class ResumeComponent implements AfterViewInit {
-  // GDrive File Link: https://docs.google.com/uc?id=1UEY-sB2py8bNRuhru1bc5eV7IUXAwAB8
-  public RESUME_URL = 'https://drive.google.com/file/d/1UEY-sB2py8bNRuhru1bc5eV7IUXAwAB8/view?usp=sharing';
 
-  constructor(@Inject(DOCUMENT) private readonly document: Document) { }
-
-  ngAfterViewInit(): void {
-    this.document.location.href = this.RESUME_URL;
-  }
+export class ResumeComponent {
 }
